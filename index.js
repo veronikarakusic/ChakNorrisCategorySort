664b53f41533e9433a391816e3722e2a428c4084
veronikarakusic <vrakusicc@gmail.com>
2024-07-04 12:51:55 +0200

    update 2

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
+const categories = [
+    "animal", "career", "celebrity", "dev", "explicit", "fashion", 
+    "food", "history", "money", "movie", "music", "political", 
+    "religion", "science", "sport", "travel"
+];
+
+function Categories() {
+    const categorySelect = document.getElementById('category');
+    categories.forEach(category => {
+        const option = document.createElement('option');
+            option.value = category;
+            option.textContent = category.charAt(0)+ category.slice(1);
+        categorySelect.appendChild(option);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', Categories);
+
 function getJoke() {
-    const categorySelect = document.getElementById('category-select');
-    const jokeDisplay = document.getElementById('joke');
-    const selectedCategory = categorySelect.value;
+    const jokeElement = document.getElementById('joke');
+    const category = document.getElementById('category').value;
     let apiUrl = "https://api.chucknorris.io/jokes/random";
 
-    if (selectedCategory) {
-        apiUrl += `?category=${selectedCategory}`;
+    if (category) {
+        apiUrl += `?category=${category}`;
     }
 
     fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             const joke = data.value;
-            jokeDisplay.textContent = joke;
+            jokeElement.textContent = joke;
             console.log(joke);
         })
-        .catch(error => {
-            console.error("Error fetching joke:", error);
+        .catch((error) => {
+            console.error("Error: ", error);
         });
 }
